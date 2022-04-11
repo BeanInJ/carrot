3a5a7a4cedfe9898f6b9e1244edc396a12783e67
yahon <yahon>
2022-04-11 18:04:28 +0800

    测试Aop

--- src/main/java/com/vegetables/system/baseServer/TestAop.java
+++ src/main/java/com/vegetables/system/baseServer/TestAop.java
@@ -3,7 +3,7 @@ package com.vegetables.system.baseServer;
 import com.vegetables.system.aop.active.AOP;
 import com.vegetables.system.aop.active.AOPBefore;
 
-@AOP("")
+@AOP("com.vegetables.system.baseServer.CarrotController.getCarrot")
 public class TestAop {
     @AOPBefore
     public void s(){
