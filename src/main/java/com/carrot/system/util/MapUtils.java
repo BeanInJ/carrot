5754502e410f46dc8ea3da0370073d3d974a6635
YaHon <YaHon@int.com>
2022-04-17 23:30:46 +0800

    项目重构 - 0417

--- src/main/java/com/carrot/system/util/MapUtils.java
+++ src/main/java/com/carrot/system/util/MapUtils.java
@@ -1,14 +1,12 @@
-package com.vegetables.util;
+package com.carrot.system.util;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MapUtils {
     // 复制map    参数：源map， 返回新map
     public static Map<String, String> copyMap(Map<String, String> sourceMap) {
-        Map<String, String> targetMap = new HashMap<String, String>();
+        Map<String, String> targetMap = new HashMap<>();
         for (String key : sourceMap.keySet()) {
             targetMap.put(key, sourceMap.get(key));
         }
