49cb26e68ea6c0fb39e066a6941a6efc3691cc1c
YaHon <YaHon@int.com>
2022-05-01 19:05:05 +0800

    优化

--- carrot/src/main/java/com/easily/cache/Session.java
+++ carrot/src/main/java/com/easily/cache/Session.java
@@ -10,16 +10,17 @@ import java.util.concurrent.ConcurrentMap;
  * Session 缓存
  * 1、超时后清理(24小时)
  * 2、可被redis或其他缓存替换
+ * 3、session是静态、全局可获取的
  */
 public class Session {
     // 保存结构 {uuid，[data,time]}
-    private final ConcurrentMap<String, Object[]> map = new ConcurrentHashMap<>();
-    private final static long MAX_TIME = 1000 * 60 * 60 * 24;
+    private static final ConcurrentMap<String, Object[]> map = new ConcurrentHashMap<>();
+    private static final long MAX_TIME = 1000 * 60 * 60 * 24;
 
     /**
      * 保存session
      */
-    public String put(Object object){
+    public static String put(Object object){
         // 判断object是否为空
         if(StringUtils.isBlankOrNull(object)) return null;
         String key = UUID.randomUUID().toString().trim().replaceAll("-", "");
@@ -31,7 +32,7 @@ public class Session {
     /**
      * 获取session
      */
-    public Object get(String key){
+    public static Object get(String key){
         Object[] objects = map.get(key);
         if(objects == null) return null;
         // 超过24小时则清理
@@ -43,7 +44,7 @@ public class Session {
         return objects[0];
     }
 
-    public <T> T get(String key, Class<T> expectedType){
+    public static  <T> T get(String key, Class<T> expectedType){
         Object object = get(key);
         if(object == null) return null;
         return expectedType.cast(object);
